acc37cc790b66f431acf6c3ca7a32606a95fdbfb
sivasenko <sivasenko@gvcgroup.com>
2019-08-28 17:42:15 +0200

    New problems

--- src/largest_prime.erl
+++ src/largest_prime.erl
@@ -13,29 +13,30 @@
 -export([run/1]).
 
 run(Number) ->
-  Primes = prime_gen(Number),
+  Primes = prime_gen(2, 1000),
   get_prime_factors(Number, Primes, []).
 
 get_prime_factors(1, _, Acc) ->
   Acc;
 get_prime_factors(Number, Primes, Acc) ->
-  {Prime, Rest} = get_first_prime(Number, Primes),
+  {Prime, Rest} = get_lowest_prime(Number, Primes),
   get_prime_factors(Rest, Primes, [Prime|Acc]).
 
 
-get_first_prime(Number, [Prime|T]) ->
+get_lowest_prime(Number, [Prime|T]) ->
   case Number rem Prime of
     0 -> {Prime, Number div Prime};
-    _ -> get_first_prime(Number, T)
-  end.
+    _ -> get_lowest_prime(Number, T)
+  end;
+get_lowest_prime(Number, []) ->
 
-prime_gen(UpperBound) ->
-  filter_primes(lists:seq(2, UpperBound), [], UpperBound).
+prime_gen(From, To) ->
+  filter_primes(lists:seq(From, To), [], To).
 
-filter_primes([X|T], Acc, UpperBound) when X > 1, X * X =< UpperBound ->
+filter_primes([X|T], Acc, MaxValue) when X > 1, X * X =< MaxValue ->
   XPrimed = [E || E <- T, E rem X /= 0],
-  filter_primes(XPrimed, [X|Acc], UpperBound);
-filter_primes([X|T], Acc, UpperBound) when X * X > UpperBound ->
+  filter_primes(XPrimed, [X|Acc], MaxValue);
+filter_primes([X|T], Acc, MaxValue) when X * X > MaxValue ->
   lists:merge(Acc, [X|T]);
 filter_primes([], Acc, _) ->
   Acc.
