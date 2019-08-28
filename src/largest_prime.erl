5150f513a5f3d65908d29a0be905951a0a6c8b7b
sivasenko <sivasenko@gvcgroup.com>
2019-08-28 18:07:27 +0200

    New problems

--- src/largest_prime.erl
+++ src/largest_prime.erl
@@ -13,22 +13,29 @@
 -export([run/1]).
 
 run(Number) ->
-  Primes = prime_gen(2, 1000),
-  get_prime_factors(Number, Primes, []).
+  UpperBound = 100,
+  Primes = prime_gen(2, UpperBound),
+  get_prime_factors(Number, {Primes,  UpperBound}, []).
 
 get_prime_factors(1, _, Acc) ->
   Acc;
-get_prime_factors(Number, Primes, Acc) ->
-  {Prime, Rest} = get_lowest_prime(Number, Primes),
-  get_prime_factors(Rest, Primes, [Prime|Acc]).
-
+get_prime_factors(Number, {Primes, UpperBound}, Acc) ->
+  case get_lowest_prime(Number, Primes) of
+    {none, none} ->
+      NewUpperBound = UpperBound + 100,
+      NewPrimes = lists:merge(Primes, prime_gen(UpperBound, NewUpperBound)),
+      get_prime_factors(Number, {NewPrimes, NewUpperBound}, Acc);
+    {Prime, Rest} ->
+      get_prime_factors(Rest, {Primes, UpperBound}, [Prime|Acc])
+  end.
 
 get_lowest_prime(Number, [Prime|T]) ->
   case Number rem Prime of
     0 -> {Prime, Number div Prime};
     _ -> get_lowest_prime(Number, T)
   end;
-get_lowest_prime(Number, []) ->
+get_lowest_prime(_, []) ->
+  {none, none}.
 
 prime_gen(From, To) ->
   filter_primes(lists:seq(From, To), [], To).
