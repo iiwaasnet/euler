5e4e46190392886e32c83c88502a16801b7144de
sivasenko <sivasenko@gvcgroup.com>
2019-08-28 11:49:34 +0200

    New problems

--- src/largest_prime.erl
+++ src/largest_prime.erl
@@ -13,16 +13,31 @@
 -export([run/1]).
 
 run(Number) ->
-  .
+  Primes = prime_gen(Number),
+  get_prime_factors(Number, Primes, []).
+
+get_prime_factors(1, _, Acc) ->
+  Acc;
+get_prime_factors(Number, Primes, Acc) ->
+  {Prime, Rest} = get_first_prime(Number, Primes),
+  get_prime_factors(Rest, Primes, [Prime|Acc]).
+
+
+get_first_prime(Number, [Prime|T]) ->
+  case Number rem Prime of
+    0 -> {Prime, Number div Prime};
+    _ -> get_first_prime(Number, T)
+  end.
 
 prime_gen(UpperBound) ->
-  [H|T] = lists:seq(2, UpperBound),
-  filter(H, [H|T], []).
+  filter_primes(lists:seq(2, UpperBound), [], UpperBound).
 
-filter(X, [X|T], Acc) ->
-  XPrimed = lists:filter(fun(E) -> E rem X /= 0 end),
-  case XPrimed of
-    [] -> []
-  end
+filter_primes([X|T], Acc, UpperBound) when X > 1, X * X =< UpperBound ->
+  XPrimed = [E || E <- T, E rem X /= 0],
+  filter_primes(XPrimed, [X|Acc], UpperBound);
+filter_primes([X|T], Acc, UpperBound) when X * X > UpperBound ->
+  lists:merge(Acc, [X|T]);
+filter_primes([], Acc, _) ->
+  Acc.
 
 
