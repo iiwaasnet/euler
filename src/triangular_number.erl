8f71bd409546f70a30716c5e3b57179853c15972
iiwaasnet <givasena@gmail.com>
2019-09-02 23:08:15 +0200

    New problems

--- src/triangular_number.erl
+++ src/triangular_number.erl
@@ -12,8 +12,28 @@
 %% API
 -export([run/1]).
 
-run(DivisorsNumber) ->
-  get_triangle([1], DivisorsNumber).
+run(DivisorsCount) ->
+  get_triangle(1, 1, DivisorsCount).
+
+get_triangle(Triangle, LastNumber, DivisorsCount) ->
+  Divisors = [X || X <- gen_divisors_seq(Triangle), Triangle rem X =:= 0],
+  if
+    length(Divisors) >= DivisorsCount ->
+      {Triangle, {divisors_count, length(Divisors)}, Divisors};
+    true ->
+      %%erlang:display({Triangle, {divisors_count, length(Divisors)}}),
+      NextNumber = LastNumber + 1,
+      get_triangle(Triangle + NextNumber, NextNumber, DivisorsCount)
+  end.
+
+gen_divisors_seq(Until) ->
+  case Until rem 2 of
+    0 ->
+      [X || X <- lists:seq(1, Until)];
+    _ ->
+      [X || X <- lists:seq(1, Until), Until rem 2 /= 0]
+  end.
+
 
 get_triangle(Seq, DivisorsNumber) ->
   Triangle = lists:foldl(fun(E, A) -> A + E end, 0, Seq),
@@ -22,6 +42,7 @@ get_triangle(Seq, DivisorsNumber) ->
     length(Divisors) >= DivisorsNumber ->
       {Triangle, {divisors_count, length(Divisors)}, Divisors};
     true ->
+      %%erlang:display({Triangle, {divisors_count, length(Divisors)}}),
       get_triangle(lists:seq(1, length(Seq) + 1), DivisorsNumber)
   end.
 
