ad44e596ba9a9a73f1879ac6958baab55851f11b
iiwaasnet <givasena@gmail.com>
2019-09-07 22:24:50 +0200

    New problems

--- src/lattice_path_15.erl
+++ src/lattice_path_15.erl
@@ -13,16 +13,53 @@
 -export([run/1]).
 
 run(GridSize) ->
-  length(span({1, 1}, GridSize)).
+  {ok, Count} = maps:find({GridSize, GridSize}, span(#{{0, 0} => 1}, GridSize)),
+  Count.
 
 span(Nodes, GridSize) ->
-  NewNodes = lists:append(
-    [{R + 1, C} || {R, C} <- Nodes, R < GridSize],
-    [{R, C + 1} || {R, C} <- Nodes, C < GridSize]
-  ),
-  case NewNodes of
-    [] ->
+  NewNodes = map_new_nodes(maps:to_list(Nodes), GridSize, #{}),
+  case maps:size(NewNodes) of
+    0 ->
       Nodes;
     _ ->
       span(NewNodes, GridSize)
+  end.
+
+map_new_nodes([], _, Acc) ->
+  Acc;
+map_new_nodes([{{R, C}, Count} | T], GridSize, Acc) ->
+  map_new_nodes(T,
+    GridSize,
+    map_new_col(
+      {{R, C}, Count},
+      GridSize,
+      map_new_row({{R, C}, Count}, GridSize, Acc))
+  ).
+
+map_new_row({{R, C}, Count}, GridSize, Acc) ->
+  if
+    R < GridSize ->
+      NewR = R + 1,
+      case maps:find({NewR, C}, Acc) of
+        {ok, Cnt} ->
+          Acc#{{NewR, C} := Cnt + Count};
+        _ ->
+          Acc#{{NewR, C} => Count}
+      end;
+    true ->
+      Acc
+  end.
+
+map_new_col({{R, C}, Count}, GridSize, Acc) ->
+  if
+    C < GridSize ->
+      NewC = C + 1,
+      case maps:find({R, NewC}, Acc) of
+        {ok, Cnt} ->
+          Acc#{{R, NewC} := Cnt + Count};
+        _ ->
+          Acc#{{R, NewC} => Count}
+      end;
+    true ->
+      Acc
   end.
